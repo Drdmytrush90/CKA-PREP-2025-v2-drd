8bef7f4bbf6af49e29bdb5d1f688bd9298d4e320
Drdmytrush90 <ddmytrush@gmail.com>
2026-07-19 20:12:22 -0500

    Fix Question-01 HPA text: target Deployment is apache-server

--- Question-01 HPA/Questions.bash
+++ Question-01 HPA/Questions.bash
@@ -1,10 +1,10 @@
 # Question HPA
-# Create a new HorizontalPodAutoScaler(HPA) named apache-server in the autoscale namespace
+# Create a new HorizontalPodAutoscaler (HPA) named apache-server in the autoscale namespace.
 
 # Task
-# 1. The HPA must target the existing deployment called apache-deployment in the autoscale namespace
-# 2. Set the HPA to target for 50% CPU usage per Pod
-# 3. Configure the HPA to have a minimum of 1 pod and a maximum of 4 pods
+# 1. The HPA must target the existing Deployment called apache-server in the autoscale namespace
+# 2. Set the HPA to aim for 50% CPU usage per Pod
+# 3. Configure it to have at least 1 pod and no more than 4 pods
 # 4. Set the downscale stabilization window to 30 seconds
 
 # Video Link - https://youtu.be/YGkARVFKtmM
